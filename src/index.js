693b9e1196b88b53e35adf0905fedb189ce20c98
mike moore <mike.moore1@mercer.com>
2019-07-24 10:54:59 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+import modTest from "./module";
+
+var thing = document.createElement('div');
+thing.innerText = "thi ddd ks"
+
+document.body.appendChild(thing);
+
+modTest(); 
---
+++ src/module.js
@@ -0,0 +1,5 @@
+export default function() {
+    var thing = document.createElement('div');
+    thing.innerText = '<div>ag</ffdiv>'
+    document.body.appendChild(thing);
+}
